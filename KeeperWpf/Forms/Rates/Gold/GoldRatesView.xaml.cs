baaa72ad591b698fb79d31d5ad5aee24c4d34c40
mli2805 <mli2805@outlook.com>
2026-05-18 08:38:22 +0300

    Fix link navigation according new .net rules

--- KeeperWpf/Forms/Rates/Gold/GoldRatesView.xaml.cs
+++ KeeperWpf/Forms/Rates/Gold/GoldRatesView.xaml.cs
@@ -14,10 +14,16 @@ public partial class GoldRatesView
         InitializeComponent();
     }
 
-    private void HandleLinkClick(object sender, RoutedEventArgs e) {  
-        Hyperlink hl = (Hyperlink)sender;  
-        string navigateUri = hl.NavigateUri.ToString();  
-        Process.Start(new ProcessStartInfo(navigateUri));  
-        e.Handled = true;  
-    } 
+    private void HandleLinkClick(object sender, RoutedEventArgs e)
+    {
+        var hl = (Hyperlink)sender;
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = hl.NavigateUri.AbsoluteUri,
+            UseShellExecute = true
+        });
+
+        e.Handled = true;
+    }
 }
